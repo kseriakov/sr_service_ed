01821c77ca735eabef7edce4c7f76469487ada6e
Konstantin S <infohakhak@gmail.com>
2022-05-30 22:00:39 +0700

    Commit 30.05.2022

--- src/sc_service/views.py
+++ src/sc_service/views.py
@@ -6,4 +6,4 @@ def home(request):
     time_now = datetime.datetime.now().date()
     name = 'Every'
     context = {'time': time_now, 'name': name}
-    return render(request, 'home.html', context=context)
+    return render(request, 'base.html', context=context)
