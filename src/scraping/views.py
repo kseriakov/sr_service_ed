7f91ace22d9258dac0041211d9bd8e64bd040ca2
Konstantin S <infohakhak@gmail.com>
2022-05-31 22:14:42 +0700

    Commit 31.05.2022

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
 from .models import *
+from .forms import *
 
 
 def home_view(request):
-    qs = Vacancy.objects.all()
-    return render(request, 'scraping/home.html', {'object_list': qs})
+    form = FindForm()
+    qs = []
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    print(city, language)
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+        qs = Vacancy.objects.filter(**_filter)
+
+    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
